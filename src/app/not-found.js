bb6992cad2e621defa566732c6f6cec4223cd962
AadiXC0DE <aadityaz2077@gmail.com>
2023-07-19 01:10:17 +0530

    added custom 404 page

---
+++ src/app/not-found.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+import image1 from "../../public/image1.png";
+
+export default function NotFound() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="flex  items-center">
+          <h1 className="text-6xl">Not found – 404!</h1>
+          <div>
+            <Image src={image1} width={450} height={450} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
