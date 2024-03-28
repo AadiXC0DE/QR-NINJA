ad46a0ce47beed641d61c915155d9a45387c57b9
AadiXC0DE <aadityaz2077@gmail.com>
2024-03-28 19:15:11 +0530

    fix component overflow in mobile view

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useCallback } from "react";
 import Particles from "react-particles";
-//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 import { loadSlim } from "tsparticles-slim";
 
 export default function Page() {
@@ -14,10 +13,6 @@ export default function Page() {
   const [qrValue, setQrValue] = useState("");
 
   const particlesInit = useCallback(async (engine) => {
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    //await loadFull(engine);
     await loadSlim(engine);
   }, []);
 
@@ -26,13 +21,12 @@ export default function Page() {
   }, []);
 
   const handleClick = () => {
-    // Store the QR code data in localStorage before setting it as the QR code value
     toast("QR created! Visit dashboard to download.");
     if (typeof window !== "undefined") {
       const qrData = localStorage.getItem("qrData")
         ? JSON.parse(localStorage.getItem("qrData"))
         : [];
-      qrData.push({ data: inputValue, date: new Date() }); // Store date with each QR code
+      qrData.push({ data: inputValue, date: new Date() });
       localStorage.setItem("qrData", JSON.stringify(qrData));
     }
     setQrValue(inputValue);
@@ -46,11 +40,8 @@ export default function Page() {
         loaded={particlesLoaded}
         options={{
           fpsLimit: 120,
-
           particles: {
-            color: {
-              value: "#ffffff",
-            },
+            color: { value: "#ffffff" },
             links: {
               color: "#ffffff",
               distance: 150,
@@ -61,30 +52,15 @@ export default function Page() {
             move: {
               direction: "none",
               enable: true,
-              outModes: {
-                default: "bounce",
-              },
+              outModes: { default: "bounce" },
               random: false,
               speed: 2,
               straight: false,
             },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 60,
-            },
-            opacity: {
-              value: 0.8,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              random: true,
-              value: 5,
-            },
+            number: { density: { enable: true, area: 800 }, value: 60 },
+            opacity: { value: 0.8 },
+            shape: { type: "circle" },
+            size: { random: true, value: 5 },
           },
           detectRetina: true,
         }}
@@ -103,13 +79,15 @@ export default function Page() {
         theme="dark"
       />
       <div className="bg-black text-white flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-8xl mb-20">QR Ninja</h1>
+        <h1 className="text-6xl md:text-8xl mb-10 md:mb-20 text-center">
+          QR Ninja
+        </h1>
         {qrValue && <QRCode value={qrValue} size={256} />}
-        <div className="flex mt-4 mb-64">
+        <div className="flex flex-col md:flex-row items-center mt-4 mb-4 md:mb-12">
           <input
             type="text"
             placeholder="Enter link/data here"
-            className="mr-2 p-1 text-black"
+            className="mr-0 md:mr-2 mb-2 md:mb-0 p-1 text-black w-full md:w-auto"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
           />
@@ -132,7 +110,7 @@ export default function Page() {
               a.download = "QRCode.png";
               a.click();
             }}
-            className="mt-4 bg-white p-1 px-4 text-black hover:bg-gray-200 transition-all"
+            className="bg-white p-1 px-4 text-black hover:bg-gray-200 transition-all"
           >
             Download QR Code
           </button>
